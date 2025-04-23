f65f4a93b5cc6ea559a1c677356762354eacf563
Illia Kalinin <ikalinin911@gmail.com>
2025-04-23 15:48:06 +0200

    SQLite, EF

--- EkgAnalysisPlatform.Api/Program.cs
+++ EkgAnalysisPlatform.Api/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite("Data Source=ekganalysis.db"));
 
 // Register repositories
 builder.Services.AddScoped<IEkgSignalRepository, EkgSignalRepository>();
